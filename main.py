369ce3dae0e69612bcacc401eb0fe92e80a80a90
usandhyarani <196360941+usandhyarani@users.noreply.github.com>
2025-02-14 00:07:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 with open('iris_model.pkl','rb') as f:
   model = pickle.load(f)
-app = FastAPi()
-classIrisInput(Basemodel):
-sl:float
-sw:float
-pl:float
-pw:float
+app = FastAPI()
+class IrisInput(Basemodel):
+  sl:float
+  sw:float
+  pl:float
+  pw:float
 @app.get("/")
 def read_root():
   return{'message':'welcome to fastapi'}
 
 @app.post("/predict/")
 def predict(data:IrisInput):
-  Input_data = [[data.sl,data.sw,data.pl,data.pw]]
-  prediction = model.predict(inputdata)[0]
-  return {prediction is ':int(prdiction)}
+  input_data = [[data.sl,data.sw,data.pl,data.pw]]
+  prediction = model.predict(input_data)[0]
+  return {'prediction is ':int(prdiction)}
